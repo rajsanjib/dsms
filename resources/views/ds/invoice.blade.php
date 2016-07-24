732ad823d5ca74cfa735196d88b02316cc29a486
yudeep-rajbhandari <endlessveins@gmail.com>
2016-07-24 18:44:40 +0545

    add invoice

--- resources/views/ds/invoice.blade.php
+++ resources/views/ds/invoice.blade.php
@@ -1,50 +1,117 @@
-@extends('includes.master')
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-@section('title','Invoice')
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-@section('navbar')
-    @include('includes.navbar')
-@endsection
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 
-@section('content')
-    @parent
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Product Id</th>
-          <th>Product Name</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      <tbody>
-          <?php
-            $purchaseSum = 0;
-          ?>
-          @foreach ($purchases as $purchase)
-              <?php
-                $Price = $purchase->quantity * $purchase->price;
-              ?>
-        <tr>
-          <td>{{ $purchase->id }}</td>
-          <td>{{ $purchase->product_name }}</td>
-          <td>{{ $purchase->selling_price }}</td>
-          <td>{{ $purchase->quantity }}</td>
-          <td>{{ $price}}</td>
-        </tr>
-        <?php
-            $purchaseSum = $purchaseSum + $price;
-        ?>
-        @endforeach
-      </tbody>
-    </table>
+<style>
+
+
+
+.invoice-title h2, .invoice-title h3 {
+    display: inline-block;
+}
+
+.table > tbody > tr > .no-line {
+    border-top: none;
+}
+
+.table > thead > tr > .no-line {
+    border-bottom: none;
+}
+
+.table > tbody > tr > .thick-line {
+    border-top: 2px solid;
+}
+
+</style>
+</head>
+<body>
+
+<div class="container">
     <div class="row">
-        <ul>
-            <li> Total: {{$purchaseSum}} </li>
-            <li> VAT (13%): {{0.13 * $purchaseSum}}</li>
-            <li> Grand Total: {{$purchaseSum + 0.13*$purchaseSum}}
+        <div class="col-xs-12">
+        <div class="invoice-title">
+          <h2>Invoice</h2><h3 class="pull-right">Order # 12345</h3>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-xs-6">
+            <address>
+            <strong>Billed To:</strong><br>
+              John Smith<br>
+              
+            </address>
+          </div>
+          
+        </div>
+        <div class="row">
+          
+          <div class="col-xs-6 text-right">
+            <address>
+              <strong>Order Date:</strong><br>
+              March 7, 2014<br><br>
+            </address>
+          </div>
+        </div>
+      </div>
     </div>
+    
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title"><strong>Order summary</strong></h3>
+          </div>
+          <div class="panel-body">
+            <div class="table-responsive">
+              <table class="table table-condensed">
+                <thead>
+                                <tr>
+                      <td><strong>Item</strong></td>
+                      <td class="text-center"><strong>Price</strong></td>
+                      <td class="text-center"><strong>Quantity</strong></td>
+                      <td class="text-right"><strong>Totals</strong></td>
+                                </tr>
+                </thead>
+                <tbody>
+                  <!-- foreach ($order->lineItems as $line) or some such thing here -->
+                  <tr>
+                    <td>BS-200</td>
+                    <td class="text-center">$10.99</td>
+                    <td class="text-center">1</td>
+                    <td class="text-right">$10.99</td>
+                  </tr>
+                                
+                  </tr>
+                  <tr>
+                    <td class="thick-line"></td>
+                    <td class="thick-line"></td>
+                    <td class="thick-line text-center"><strong>Subtotal</strong></td>
+                    <td class="thick-line text-right">$670.99</td>
+                  </tr>
+                  
+                  <tr>
+                    <td class="no-line"></td>
+                    <td class="no-line"></td>
+                    <td class="no-line text-center"><strong>Total</strong></td>
+                    <td class="no-line text-right">$685.99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</body>
+</html>
 
-@endsection
